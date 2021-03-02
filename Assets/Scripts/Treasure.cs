89ba4f9cb589383f5f3236b6bd208d6cdf005f7a
vy538 <good5538@gmail.com>
2021-03-02 08:26:31 +0800

    hover UI

--- Assets/Scripts/Treasure.cs
+++ Assets/Scripts/Treasure.cs
@@ -7,7 +7,10 @@ public class Treasure : MonoBehaviour
 {
 
     // public GameObject treasureButton;
+    public GameObject desc;
     public Text text;
+    public Canvas myCanvas;
+    public Vector3 offset;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,19 +22,22 @@ public class Treasure : MonoBehaviour
         
     }
 
-    public void WhenMouseOn(GameObject button)
+    public void WhenMouseOn(string msg)
     {
-        text = button.GetComponentInChildren<Text>();
         //If your mouse hovers over the GameObject with the script attached, output this message
         // Debug.Log("Mouse is over GameObject.");
-        Debug.Log(button.name == "boost_btn0");
-        text.enabled = true;
+        desc.SetActive(true);
+
+        Vector2 pos;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(myCanvas.transform as RectTransform, Input.mousePosition + offset, myCanvas.worldCamera, out pos);
+        desc.transform.position = myCanvas.transform.TransformPoint(pos);
+        
+        text.text = msg;
     }
 
-    public void WhenMouseExit(GameObject button)
+    public void WhenMouseExit()
     {
-        text = button.GetComponentInChildren<Text>();
-        text.enabled = false;
+        desc.SetActive(false);
     }
 
 }
